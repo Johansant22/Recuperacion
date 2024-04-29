b0c7c875eff5414303a02a76e2a196cf9c297c58
Johansant22 <johansan799@gmail.com>
2024-04-29 16:36:17 -0500

    recuperacion

---
+++ app/src/main/java/com/sss/calculadoraee/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.sss.calculadoraee
+
+import android.icu.lang.UCharacter.GraphemeClusterBreak.V
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.sss.calculadoraee.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private var operador: String = ""
+    private var num1: String = ""
+    private var num2: String = ""
+    private lateinit var txtcaja: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val buttons = arrayOf(
+            binding.btnuno,
+            binding.btndos,
+            binding.btntres,
+            binding.btncua,
+            binding.btncinco,
+            binding.btnseis,
+            binding.btnsie,
+            binding.btnoch,
+            binding.btnnue,
+            binding.cero,
+            binding.btnMas,
+            binding.btnmenos,
+            binding.btnMult,
+            binding.btndiv,
+            binding.btnigual,
+            binding.btnElimNum,
+            binding.btnElimTodo
+        )
+
+        buttons.forEach { button ->
+            button.setOnClickListener(this)
+        }
+        txtcaja = binding.txtcaja
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.cero -> ingresaNumber("0")
+            R.id.btnuno -> ingresaNumber("1")
+            R.id.btndos -> ingresaNumber("2")
+            R.id.btntres -> ingresaNumber("3")
+            R.id.btncua -> ingresaNumber("4")
+            R.id.btncinco -> ingresaNumber("5")
+            R.id.btnseis -> ingresaNumber("6")
+            R.id.btnsie -> ingresaNumber("7")
+            R.id.btnoch -> ingresaNumber("8")
+            R.id.btnnue -> ingresaNumber("9")
+            R.id.btnMas -> operadorMath("+")
+            R.id.btnmenos -> operadorMath("-")
+            R.id.btnMult -> operadorMath("*")
+            R.id.btndiv -> operadorMath("/")
+            R.id.btnigual -> calculateResult()
+            R.id.btnElimNum -> borrar (V)
+            R.id.btnElimTodo -> clear(V)
+
+        }
+    }
+    private fun ingresaNumber(number: String) {
+        binding.txtcaja.append(number)
+    }
+    private fun operadorMath(operator: String) {
+        num1 = binding.txtcaja.text.toString()
+        binding.txtcaja.setText("")
+        this.operador = operator
+    }
+    private fun calculateResult() {
+        num2 = binding.txtcaja.text.toString()
+        var resultado = 0.0
+        when (operador) {
+            "+" -> resultado = num1.toDouble() + num2.toDouble()
+            "-" -> resultado = num1.toDouble() - num2.toDouble()
+            "*" -> resultado = num1.toDouble() * num2.toDouble()
+            "/" -> resultado = num1.toDouble() / num2.toDouble()
+        }
+        binding.txtcaja.setText(resultado.toString())
+    }
+    fun borrar(view: Int) {
+        var cadena = txtcaja.text.toString()
+        if (cadena.isNotEmpty()) {
+            cadena = cadena.substring(0, cadena.length - 1)
+            txtcaja.setText(cadena)
+        }
+    }
+    fun clear(view: Int) {
+        txtcaja.setText("")
+    }
+}
